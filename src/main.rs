aefc78ee86f2070f96e6cfa71051e9e13afafcf5
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-24 15:54:45 -0400

    add obstacle struct and implementation

--- src/main.rs
+++ src/main.rs
@@ -144,3 +144,20 @@ impl Player {
         self.velocity = -2.0;
     }
 }
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score)
+        }
+    }
+}
