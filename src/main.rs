69b7e1461eb943f6c728e345fa2f826208ccb661
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-24 14:11:50 -0400

    initial commit with game state struct

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, bracket terminal")
+    }
+}
