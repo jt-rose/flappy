374f5c182c473401e537ba1177b272d42dca36de
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-24 16:18:21 -0400

    update game state management for obstacle calculations

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ struct State {
     mode: GameMode,
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
+    score: i32
 }
 
 impl State {
@@ -33,6 +35,8 @@ impl State {
             mode: GameMode::Menu,
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0
         }
     }
 
@@ -50,6 +54,19 @@ impl State {
 
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+            self.mode = GameMode::End;
+        }
+
         if self.player.y > SCREEN_HEIGHT {
             self.mode = GameMode::End;
         }
@@ -59,6 +76,8 @@ impl State {
         self.mode = GameMode::Playing;
         self.frame_time = 0.0;
         self.player = Player::new(5, 25);
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -79,6 +98,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
@@ -161,7 +181,7 @@ impl Obstacle {
     }
 
     fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player.x;
+        let screen_x = self.x - player_x;
         let half_size = self.size / 2;
 
         // draw the top portion of an obstacle
