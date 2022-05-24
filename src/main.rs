6a1bedef96c1680d068bcee72aa480886742e107
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-24 15:31:17 -0400

    add player struct and render method

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,105 @@
 
 use bracket_lib::prelude::*;
 
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
 fn main() -> BError {
 
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
 
-struct State {}
+struct State {
+    mode: GameMode
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu
+        }
+    }
+
+    fn play(&mut self, _ctx: &mut BTerm) {
+        // TODO: fill in later
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        // TODO: fill in later
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {},
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {},
+            }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, bracket terminal")
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
+    }
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
     }
 }
